3a5b99f7d88cb8f58ce2d87dfa925f7987850231
Aung Khant Zin <aungkhantzin881@gmail.com>
2024-03-29 16:37:17 +0630

    Search

--- src/components/Searchbar.js
+++ src/components/Searchbar.js
@@ -1,22 +1,35 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, TextInput, StyleSheet } from 'react-native';
 import { Feather } from '@expo/vector-icons';
 
 const SearchBar = () => {
   return (
-    <View style={styles.background}>
-      <Feather name="search" size={30}/>
-      <Text>Search Bar</Text>
+    <View style={styles.backgroundStyle}>
+      <Feather style={styles.iconStyle} name="search"/>
+      <TextInput style={styles.inputStyle}
+        placeholder='Search'
+      />
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-    background: {
-        backgroundColor: '#F0EEEF',
+    backgroundStyle: {
+        marginTop: 15,
+        backgroundColor: '#F0EEEE',
         height: 50,
         borderRadius: 5,
+        marginHorizontal: 15,
+        flexDirection: 'row'
+    },
+    iconStyle: {
+        fontSize: 35,
+        alignSelf: 'center',
         marginHorizontal: 15
+    }, 
+    inputStyle: {
+        flex: 1,
+        fontSize: 18
     }
 });
 
